8f381e174f0d2ce7bf0f58d051e8331613e7db9e
Subha Moneesh Kasireddi <subhamoneeshkasireddi@Subhas-MacBook-Air.local>
2025-10-15 23:36:41 +0530

    first commit

---
+++ src/main/java/com/example/customers_data/CustomersDataApplication.java
@@ -0,0 +1,13 @@
+package com.example.customers_data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomersDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomersDataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/customers_data/CustomersDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.customers_data;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomersDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
